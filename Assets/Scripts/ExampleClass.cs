69e3bca684230bd7fbc0682f30fe3aca1b2e3d90
Andrey Safronov <andrey.schule@gmail.com>
2020-11-09 14:11:20 +0100

    Ein Script wurde hinzugefügt.

---
+++ Assets/Scripts/ExampleClass.cs
@@ -0,0 +1,23 @@
+﻿// Bibliotheken / Asembly Referenz / Namespaces
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+// Deklaration einer Klasse
+
+// public           -> access modifier (Zugriffsrechte) / Keyword (vorreservierte Schlüssel)
+// class            -> Keyword zur Deklaration einer Klasse
+//ExampleClass      -> identifier (Name der Klasse)
+// : Monobehaviour  -> Erstellen einer Unterklasse von MonoBehaviour
+// {}               -> Scope/Wirkungsbereich (Was ist in der Klasse enthalten) 
+
+
+// Klasse: Säugetiere -> Unterklasse: Hund
+//                       Unterklasse: Katze
+
+public class ExampleClass : MonoBehaviour
+{
+
+}
